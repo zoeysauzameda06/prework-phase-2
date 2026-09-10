b44a2a231ac900a43dff9c20755ed5b82f5f8ac6
zoeysauzameda06 <zsauzamedavega@student.cnm.edu>
2026-09-10 09:11:21 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -15,11 +15,20 @@ function initColorPicker() {
 function setColorPickerEventListeners(colorBox, rgb) {
     rgb.red.addEventListener('change', () => {
         console.log("Red value: ", rgb.red.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.green.addEventListener('change', () => {
         console.log("Green value: ", rgb.green.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
     rgb.blue.addEventListener('change', () => {
         console.log("Blue value: ", rgb.blue.value);
+        setBoxBGColor(colorBox, rgb.red.value, rgb.green.value, rgb.blue.value);
     });
+}
+
+function setBoxBGColor(colorBox, red, green, blue) {
+    rgbVal = [red, green, blue].join(',');
+    colorBox.style.backgroundColor = "rgb(" + rgbVal + ")";
+
 }
