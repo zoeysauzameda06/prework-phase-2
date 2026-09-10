633105145db6403ab0f4b31a959409a3ccc291f3
zoeysauzameda06 <zsauzamedavega@student.cnm.edu>
2026-09-09 18:29:16 -0600

    Add files via upload

---
+++ objects-apis.js
@@ -0,0 +1,27 @@
+/*
+ *objects, interfaces, and API'S
+ */
+/*
+let dog = {
+    name: "Steve",
+    color: "brown",
+    breed: "Pug",
+    size: "Small",
+    bark: function(TypeOfBark){
+        console.log("Bark!");
+    },
+};
+*/
+/*
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "Tom,",
+    num: 10,
+};
+x(y);
+console.log(y);
+ */
